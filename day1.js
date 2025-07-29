f6e57f154686a1f22f7c62145cfb92f8ccd832f4
lenovo pc <vijayabharathikaliraj@gmail.com$   git config --global user.email vijayabharathikaliraj@gmail.comgit config --global user.email vijayabharathikaliraj@gmail.com>
2025-07-29 10:39:07 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,14 @@
+console.log("gfgdgdgdhg");
+let b=[20,45,30,67,78];
+console.log(b[2]);
+b[2]=77;
+console.log(b);
+b[6]=66;
+console.log(b);
+ 
+let obj={firstworldwar:'1914', secondworldwar:'1939', germansurrenders:'1945', economiesimpacts:'2008'};
+console.log(obj);
+
+// let a=1112;
+// let c=12;
+// console.log(a);
